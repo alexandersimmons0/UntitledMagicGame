c1fb5c2bd20fff68b98b7379469fe04b904f5dd7
alexandersimmons0 <simmoa14@my.erau.edu>
2024-09-25 18:21:11 -0700

    WebBuildFinally
    Web build with functional inventory and basic movement system

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,9 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerInventory : MonoBehaviour{
+    public class InventoryObject{
+        public int objID;
+        public int objCount;
+        public string objName;        
+    }
+    int i;
+    public List<InventoryObject> objects = new List<InventoryObject>();
 
+    public void CollectObject(GameObject collectable){
+        if(!objects.Any()){
+            objects.Add(new InventoryObject() { objName = collectable.name, objCount = 1});
+        }else if(!objects.Exists(x => x.objName == collectable.name)){
+            objects.Add(new InventoryObject() { objName = collectable.name, objCount = 1});
+        }else{
+            //i = objects.IndexOf(new InventoryObject(){ objName = collectable.name });
+            //objects[i].objCount += 1;
+            var item = objects.FirstOrDefault(x => x.objName == collectable.name);
+            if(item != null){
+                item.objCount += 1;
+            }
+        }
+    }
+
+    public void DisplayInventory(){
+        foreach(InventoryObject Aobject in objects){
+            Debug.Log(Aobject.objName + " Amount of: " + Aobject.objCount);
+        }
+        
+    }
     //maybe change return type?
     //what inputs and outputs
     //how store and track inventory objects
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,24 @@ public class PlayerMovement : MonoBehaviour{
 
     public float speed;
     public Rigidbody rigid;
+    public PlayerInventory inventoryScript;
 
     void Update(){
         hInput = Input.GetAxis("Horizontal") * speed;
         vInput = Input.GetAxis("Vertical") * speed;
+        if(Input.GetKeyDown(KeyCode.E)){
+            inventoryScript.DisplayInventory();
+        }
     }
 
     void FixedUpdate(){
         rigid.MovePosition (transform.position + transform.forward * vInput * Time.deltaTime + transform.right * hInput * Time.fixedDeltaTime);
+    }
+
+    void OnTriggerEnter(Collider other){
+        if(other.gameObject.CompareTag("collectable")){
+            other.gameObject.SetActive(false);
+            inventoryScript.CollectObject(other.gameObject);
+        }
     }
 }
